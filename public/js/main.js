dd8655240dd51ebade6b0cc8a7e110a56b34446f
Javier Akira Luca de Tena Arai <javierakira@Javiers-MacBook-Air.local>
2017-09-21 18:09:54 +0900

    vue component example

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
+
+
+Vue.component('perro', {
+  props: ['nombre_perro'],
+  template: '<h3>Necesita más perro, en conctreto, uno que se llame {{ nombre_perro }}</h3>'
+})
+
+
+// Vue app
 var app = new Vue({
   el: '#app',
   data: {
